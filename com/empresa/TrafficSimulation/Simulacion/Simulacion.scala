42c7ff08ca2be0aa51836cb2835dc6548d018e21
Javier Dario <jdechavarriac@unal.edu.co>
2019-07-07 16:41:14 -0500

    Basic project structure

---
+++ com/empresa/TrafficSimulation/Simulacion/Simulacion.scala
@@ -0,0 +1,11 @@
+package empresa.TrafficSimulation.Simulacion
+
+object Simulacion extends Runnable{
+  def run(): Unit = {
+    while (true) {
+      //t += td
+      //Grafico.graficar
+      Thread.sleep(1000)
+    }
+  }
+}
