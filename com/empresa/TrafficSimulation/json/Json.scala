e5d4776cae2522881cd4921a7263985f0515f8f9
Javier <jdechavarriac@unal.edu.co>
2019-07-19 17:55:11 -0500

    Updated class Json

--- com/empresa/TrafficSimulation/json/Json.scala
+++ com/empresa/TrafficSimulation/json/Json.scala
@@ -1,14 +1,19 @@
+package json
+
 import scala.io.Source
 import net.liftweb.json._
 
+
 //Esta clase se encarga de leer y escribir los archivos .json con las configuraciones de la aplicación.
 
-class Json(){
+object Json{
   val currentDirectory:String = new java.io.File(".").getCanonicalPath
   val jsonRaw = Source.fromFile(s"$currentDirectory/parámetros.json").getLines.mkString
   val jsonFile:JValue = parse(jsonRaw)
 
-  implicit val formats = DefaultFormats
+  implicit val formats:Formats = DefaultFormats
+
+  val parametros:ParametrosSimulacion = jsonFile.extract[Data].parametrosSimulacion
 
   case class ParametrosSimulacion(dt: Int,
                                   tRefresh: Int,
@@ -24,5 +29,14 @@ class Json(){
 
   case class Data(parametrosSimulacion: ParametrosSimulacion)
 
-  val dt = jsonFile.extract[Data].parametrosSimulacion.dt //ejemplo de la manera en la que se extraen los datos.
+  def getMotos:Double = parametros.proporciones.motos
+  def getCarros:Double = parametros.proporciones.carros
+  def getCamiones:Double = parametros.proporciones.camiones
+  def getMotoTaxis:Double = parametros.proporciones.motoTaxis
+  def getDt:Int = parametros.dt
+  def getTRefresh:Int = parametros.tRefresh
+  def getMaximo:Int = parametros.vehiculos.maximo
+  def getMinimo:Int = parametros.vehiculos.minimo
+  def getVelMax:Int = parametros.velocidad.maximo
+  def getVelMin:Int = parametros.velocidad.minimo
 }
