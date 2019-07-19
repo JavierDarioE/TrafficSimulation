b2c268266c3bb6f3d512b1103328474ee9b5ae3f
Javier <jdechavarriac@unal.edu.co>
2019-07-19 17:55:32 -0500

    Updated class Simulacion

--- com/empresa/TrafficSimulation/simulacion/Simulacion.scala
+++ com/empresa/TrafficSimulation/simulacion/Simulacion.scala
@@ -1,10 +1,20 @@
-package empresa.TrafficSimulation.Simulacion
+package simulacion
 
-object Simulacion extends Runnable{
-  def run(): Unit = {
+import json.Json
+
+class Simulacion extends Runnable{
+  override def run(): Unit = {
+    var t = Json.getTRefresh
+    val dt = Json.getDt
+    println(s"camiones: ${Json.getCamiones}")
+    println(s"carros: ${Json.getCarros}")
+    println(s"t: $t")
+    println(s"dt: $dt")
+
     while (true) {
       //t += td
       //Grafico.graficar
+      println("thread is running")
       Thread.sleep(1000)
     }
   }
